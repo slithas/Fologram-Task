a53a88e1daf9aa379cf135865c495e2a42a11acd
Lance Mansutti <lancemansutti@gmail.com>
2019-07-10 21:38:31 +1000

    Scale Control

--- Fologram Task/Assets/CanvasMasterControl.cs
+++ Fologram Task/Assets/CanvasMasterControl.cs
@@ -1,24 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CanvasMasterControl : MonoBehaviour
 {
+    public static CanvasMasterControl cmc;
+
     public GameObject world;
     public GameObject screen;
 
-    bool swap;
+    public Transform scaler;
+    public float x = 1;
+    public float y = 1;
+    public float z = 1;
+    public bool join;   //toggle the link for all scaling on and off
+
+    bool swap = true;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        cmc = this;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        scaler.localScale = new Vector3(x, y, z);
     }
 
     //flip the active canvas
@@ -27,5 +36,17 @@ public class CanvasMasterControl : MonoBehaviour
         world.SetActive(swap);
         screen.SetActive(!swap);
         swap = !swap;
+    }
+
+    public void Scale(string n, float value)
+    {
+        if (n == "x" || join) x = value;
+        if (n == "y" || join) y = value;
+        if (n == "z" || join) z = value;
+    }
+
+    public void Toggle()
+    {
+        join = !join;
     }
 }
---
+++ Fologram Task/Assets/ScaleController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScaleController : MonoBehaviour
+{
+    public bool x;
+    public bool y;
+    public bool z;
+
+    Slider slider;
+    CanvasMasterControl cmc;
+
+    void Start()
+    {
+        cmc = CanvasMasterControl.cmc;
+        slider = GetComponent<Slider>();
+    }
+
+    // Keeping all the sliders the same value on both canvases
+    void LateUpdate()
+    {
+        if (x) slider.value = cmc.x;
+        if (y) slider.value = cmc.y;
+        if (z) slider.value = cmc.z;
+    }
+
+    public void Scale()
+    {
+        if (x) cmc.Scale("x", slider.value);
+        if (y) cmc.Scale("y", slider.value);
+        if (z) cmc.Scale("z", slider.value);
+    }
+}
