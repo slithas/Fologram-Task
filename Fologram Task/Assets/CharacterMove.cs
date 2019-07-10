6f9de8e3e28016eebee7fc3bcea6dd6dbf6b8c27
Lance Mansutti <lancemansutti@gmail.com>
2019-07-10 19:56:45 +1000

    Core Game

---
+++ Fologram Task/Assets/CharacterMove.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMove : MonoBehaviour
+{
+    public float speed = 6.0f;
+
+    void Update()
+    {
+        // Move the controller
+        transform.eulerAngles += new Vector3(0, Input.GetAxis("Horizontal"), 0) * speed;
+        if (Input.GetAxis("Vertical") > 0) transform.position += transform.forward * Time.deltaTime * speed;
+    }
+}
